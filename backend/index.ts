a19d3119fb68fa45215a71c7bb8f20e0f07f5115
theusaf <theusafyt@gmail.com>
2024-03-06 20:13:03 -0800

    feat: add basic io functions to gluon versions

--- backend/index.ts
+++ backend/index.ts
@@ -1,15 +1,18 @@
 import * as Gluon from "@gluon-framework/gluon";
 import express from "express";
 import path from "node:path";
-import fs from "node:fs";
+import { existsSync, createWriteStream } from "node:fs";
+import { readFile, writeFile, readdir, mkdir, rm } from "node:fs/promises";
 import { fileURLToPath } from "node:url";
+import archiver from "archiver";
+import yauzl from "yauzl";
 
 const dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 
-if (fs.existsSync(path.join(dirname, "../dist/"))) {
+if (existsSync(path.join(dirname, "../dist/"))) {
   app.use(express.static(path.join(dirname, "../dist")));
-} else if (fs.existsSync(path.join(dirname, "dist"))) {
+} else if (existsSync(path.join(dirname, "dist"))) {
   app.use(express.static(path.join(dirname, "dist")));
 }
 
@@ -56,3 +59,79 @@ function bindToPort(port: number) {
       });
   });
 }
+
+window.ipc.expose("save", async (data: string, id: string) => {
+  await mkdir(path.join(dirname, "saves"), { recursive: true });
+  const stream = createWriteStream(path.join(dirname, "saves", `${id}.zip`));
+  const archive = archiver("zip", {
+    zlib: { level: 9 },
+  });
+  archive.pipe(stream);
+  return new Promise((resolve) => {
+    archive.on("close", resolve);
+    archive.append(data, { name: "save.yaml" });
+    archive.finalize();
+  });
+});
+
+window.ipc.expose("setLatestSave", async (id: string) => {
+  await mkdir(path.join(dirname, "data"), { recursive: true });
+  await writeFile(path.join(dirname, "data", "latestSave"), id);
+});
+
+window.ipc.expose("getLatestSave", async () => {
+  try {
+    return await readFile(path.join(dirname, "data", "latestSave"), "utf8");
+  } catch (err) {
+    return null;
+  }
+});
+
+window.ipc.expose("load", async (id: string) => {
+  try {
+    const data = await readFile(path.join(dirname, "saves", `${id}.zip`));
+    return await new Promise((resolve, reject) => {
+      yauzl.fromBuffer(data, { lazyEntries: true }, (err, zipfile) => {
+        if (err) return reject(err);
+        zipfile.readEntry();
+        zipfile.on("entry", (entry) => {
+          if (entry.fileName !== "save.yaml") {
+            zipfile.readEntry();
+            return;
+          }
+          zipfile.openReadStream(entry, (err, readStream) => {
+            if (err) return reject(err);
+            let data = "";
+            readStream.on("data", (chunk) => {
+              data += chunk;
+            });
+            readStream.on("end", () => {
+              zipfile.close();
+              resolve(data);
+            });
+          });
+        });
+      });
+    });
+  } catch (err) {
+    return null;
+  }
+});
+
+window.ipc.expose("delete", async (id: string) => {
+  try {
+    await rm(path.join(dirname, "saves", `${id}.zip`));
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+window.ipc.expose("list", async () => {
+  try {
+    return (await readdir(path.join(dirname, "saves"))).map((file) => {
+      return file.replace(/\.zip$/, "");
+    });
+  } catch (err) {
+    return [];
+  }
+});
--- src/game/util/saves.ts
+++ src/game/util/saves.ts
@@ -9,7 +9,8 @@ export interface Saveable<T> {
 
 export async function getLatestSave<T>(): Promise<T | null> {
   if (window.Gluon) {
-    // TODO: Implement latest save
+    const saveId = await window.Gluon.ipc.getLatestSave();
+    if (saveId) return load(saveId);
   } else {
     const saveData = localStorage.getItem("latestSave");
     if (saveData && typeof saveData === "string") return load(saveData);
@@ -19,7 +20,8 @@ export async function getLatestSave<T>(): Promise<T | null> {
 
 export async function save<T>(data: Saveable<T>, id: string): Promise<void> {
   if (window.Gluon) {
-    // TODO: Implement save
+    await window.Gluon.ipc.save(data.toYAML(), id);
+    await window.Gluon.ipc.setLatestSave(id);
   } else {
     localStorage.setItem(`save/${id}`, data.toYAML());
     localStorage.setItem("latestSave", id);
@@ -27,19 +29,16 @@ export async function save<T>(data: Saveable<T>, id: string): Promise<void> {
 }
 
 export async function load<T>(id: string): Promise<T | null> {
-  if (window.Gluon) {
-    // TODO: Implement load
-    return null;
-  } else {
-    const data = localStorage.getItem(`save/${id}`);
-    if (!data) return null;
-    return parse(data);
-  }
+  const data = window.Gluon
+    ? await window.Gluon.ipc.load(id)
+    : localStorage.getItem(`save/${id}`);
+  if (!data) return null;
+  return parse(data);
 }
 
 export async function deleteSave(id: string): Promise<void> {
   if (window.Gluon) {
-    // TODO: Implement deletion
+    await window.Gluon.ipc.delete(id);
   } else {
     localStorage.removeItem(`save/${id}`);
   }
@@ -47,8 +46,7 @@ export async function deleteSave(id: string): Promise<void> {
 
 export async function getSaveList(): Promise<string[]> {
   if (window.Gluon) {
-    // TODO: Implement save list
-    return [];
+    return window.Gluon.ipc.list();
   } else {
     return Object.keys(localStorage)
       .filter((key) => key.startsWith("save/"))
