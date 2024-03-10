4bf19331872b1b495ba76cabfcb5b55438d20a9e
theusaf <theusafyt@gmail.com>
2024-03-10 10:27:18 -0700

    fix: remove typo in save app

--- src/game/screens/ui/map_screen_pages/save.tsx
+++ src/game/screens/ui/map_screen_pages/save.tsx
@@ -24,7 +24,7 @@ export function SavePagePhone({ gameManager }: { gameManager: GameManager }) {
           gameManager.changeScreen(new MainMenuScreen());
         }}
       >
-        Save and Returnt to Menu
+        Save and Return to Menu
       </TextActionButton>
       <div>{message}</div>
     </div>
